a4be788b7c57051b809506899b65ebe7dd4a88bb
jonecto <josen.rios@urosario.edu.co>
2022-10-09 09:45:10 -0500

    Update descriptor.py

--- descriptor.py
+++ descriptor.py
@@ -15,26 +15,37 @@ class Descriptor():
         self.Y=[0,1,2]
         self.pieces=['Peon','Ficha']
         self.turns=[0,1,2]
-        
-    def code(self, n_args): #n_args recibe una lista con los argumentos (pieza,x,y,turno)
-        x= self.X[n_args[1]]    #IMPORTANTE: El argumento Pieza es 0 si es peón, 1 si es ficha
-        y= self.Y[n_args[2]]
-        t= self.turns[n_args[3]]
-        tot= 200 + n_args[0] + x*2 + y*10 + t*100
-        return(chr(tot))
+ 
+    def code(self, n_args):
+        '''
+         n_args recibe una lista con los argumentos (pieza,x,y,turno)
+         IMPORTANTE: El argumento Pieza es 0 si es peón, 1 si es ficha
+        '''
+        x= self.X[n_args[1]] #Posición en x
+        y= self.Y[n_args[2]] #Posición en Y
+        t= self.turns[n_args[3]] #Turno
+        tot= 200 + n_args[0] + x*2 + y*10 + t*100 #Una operación simple
+        return(chr(tot)) #Devuelve una letra rara como È
     
     def decode(self, car):
-        i=ord(car)
-        j=i-200
-        pi=(j%10)%2
-        eq=(j%10)//2
-        ye=(j%100)//10
-        tu=j//100
+        '''
+        Recibe un caracter raro como È
+        Devuelve la información de la letra
+        '''
+        i=ord(car) #Sacar el ascii del caracter
+        j=i-200 #Quitar el exceso
+        pi=(j%10)%2 #Sacar el número de la pieza (peón o ficha)
+        eq=(j%10)//2 #Sacar la posición en X
+        ye=(j%100)//10 #Sacar la posición en Y
+        tu=j//100 #Sacar el turno
         return [pi,eq,ye,tu]
     
     def write(self,car):
-        z=self.decode(car)
-        if(z[0]==1):
+        '''
+        Escribe la letra proposicional en lenguaje natural
+        '''
+        z=self.decode(car) #Recibe los argumentos 
+        if(z[0]==1): #identifica que tipo de pieza es
             pieza="Ficha"
         else:
             pieza="Peon"
@@ -62,17 +73,23 @@ def Otoria(lista_forms):
     return form
     
 def see_rule(cadena):
-    trad=""
-    n=Descriptor()
+    '''
+    Se encarga de escribir la regla, estando esta en formato str
+    '''
+    trad="" #Para guardar el string que se devolverá
+    n=Descriptor() #Un objeto tipo descriptor para la decodificación
     for i in cadena:
-        if ord(i)>199:
-            trad+=n.write(i)
-        elif i=="-":
+        if ord(i)>199: #Revisa si es una letra rara como È
+            trad+=n.write(i) #Añade la traducción de la letra 
+        elif i=="-": #Si hay un -, añade un No al frente. 
             trad+="no "
-        else:
+        else: #Si no pasa ninguna de esas, coloca el argumento común y corriente
             trad+=i
     return trad
 '''
+
+#Si quiere observar el funcionamiento de cada cosa, quite las comillas y corra el programa
+
 prueba = descriptor()
 rev=[]
 i=0
